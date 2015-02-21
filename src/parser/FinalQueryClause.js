820010ad62a165f42df796984b558d750b6de9f1
Cristian Carlesso <kentaromiura@gmail.com>
2015-02-21 17:45:10 +0000

    change FinalQuery to handle group by

--- src/parser/FinalQueryClause.js
+++ src/parser/FinalQueryClause.js
@@ -1,15 +1,15 @@
-var SelectClause = require('./SelectClause')
+var SelectClause = require('./SelectClause'),
+    GroupByClause = require('./GroupByClause')
 var whatsNext = require('./utils/next')(['select', 'group'])
 
 //final-query-clause ::= (select-clause | groupby-clause)
 module.exports = function(mutableSource){
   switch(whatsNext(mutableSource.source)){
     case 'select':
-      return new SelectClause(mutableSource)
+      return SelectClause(mutableSource)
       break
     case 'group':
-      //todo: handle groups
-      return 'TODO'
+      return GroupByClause(mutableSource)
     default:
       throw new Error('parse error in finalQueryClause');
       break
