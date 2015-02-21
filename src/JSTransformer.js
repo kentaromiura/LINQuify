93d547c652592ffd2391b36b76cd35f9e881356c
Cristian Carlesso <kentaromiura@gmail.com>
2015-02-21 15:47:19 +0000

    weirderize the transformer to support from
    The transformer really should be rewrite using an east
    for now generating raw JS still works

--- src/JSTransformer.js
+++ src/JSTransformer.js
@@ -6,7 +6,11 @@ module.exports = function(ast){
   var from = ast.fromClause,
       body = ast.queryBody,
       clauses = body.queryBodyClauses,
-      output = [from.srcExpression]
+      output = [from.srcExpression],
+      after = [], //after everything
+      pre = []; // before a select
+      post = []; // after a select
+
   // Very basic first implementation of the transform (not using AST yet)
   if (clauses) {
     clauses.forEach(function(clause){
@@ -16,6 +20,14 @@ module.exports = function(ast){
             'return ', clause.predicate,
           '})')
           break
+        case 'FromClause':
+
+          output.unshift('[].concat.apply([],')
+          after.push(')')
+
+          pre.push(clause.srcExpression, '.map(function(',clause.itemName,'){return ')
+          post.push('})')
+          break
       default:
         throw new Error('not yet implemented')
       }
@@ -23,7 +35,11 @@ module.exports = function(ast){
   }
 
   output.push('.map(function(', from.itemName, '){',
-    'return ', body.finalQueryClause.selectExpression ,
+    'return ',
+      pre.join(''),
+      body.finalQueryClause.selectExpression ,
+      post.join(''),
   '})')
+  output.push(after.join(''))
   return output.join('')
 }
